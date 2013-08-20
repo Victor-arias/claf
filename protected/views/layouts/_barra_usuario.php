bfb57ab363884e63daa06a411d53e81c1949408e
Victor Arias <hugovic1182@gmail.com>
2013-08-20 11:09:36 -0500

    Corregido el bootstrap

--- protected/views/layouts/_barra_usuario.php
+++ protected/views/layouts/_barra_usuario.php
@@ -9,9 +9,9 @@ if(!is_null($idSesion)):
 ?>
 <div class="row">	
 	<div class="span12 well">
-		<span class="glyphicon glyphicon-home"><?php echo CHtml::link('Inicio', '/', array('class' => 'btn btn-link')) ?></span>
+		<span><?php echo CHtml::link(' Inicio', '/', array('class' => 'btn btn-link glyphicon glyphicon-home')) ?></span>
 		<span class="pull-right">
-			<strong>Jugador:</strong> <?php echo $usuarioConectado->nombre . " " . $usuarioConectado->apellido ?>
+			<strong>Jugador:</strong> <?php echo $usuarioConectado->nombre . " " . $usuarioConectado->apellido ?> | 
 			Puntaje Total: <?php echo $usuarioConectado->puntaje . ' | ' . CHtml::link('Cerrar Sesión', 'logout', array('class' => 'btn btn-danger btn-xs'))?>
 		</span>
 	</div>
