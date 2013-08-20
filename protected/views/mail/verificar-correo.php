998c2b62e684b48286739e1b7047dd6958e32d46
Belmar Santanilla Gutiérrez <belmars@gmail.com>
2013-08-20 11:19:45 -0500

    Se modificó la URL de inicio en la barra del usuario

--- protected/views/mail/verificar-correo.php
+++ protected/views/mail/verificar-correo.php
@@ -7,11 +7,12 @@
 <body bgcolor="#eee" style="background-color: #eee;">
 	<center>
 	<table  bgcolor="#fff" style="display: inline-table; margin:0 auto" border="0" cellpadding="0" cellspacing="0" width="605">
+		<!--
 		<tr>
 			<td>
 				<img src="http://concursomedellin2018.com/images/mail/header-mail.jpg" width="605" height="154" />
 			</td>
-		</tr>
+		</tr>-->
 		<tr>
 			<td>
 				<center>
@@ -23,11 +24,12 @@
 				</center>
 			</td>
 		</tr>
+		<!--
 		<tr>
 			<td>
 				<img src="http://concursomedellin2018.com/images/mail/footer-mail.jpg" width="605" height="113" />
 			</td>
-		</tr>
+		</tr>-->
 	</table>
 	</center>
 </body>
